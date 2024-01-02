c5a65e363e63195a15d08b45f6a6d5080f9730d3
Khurram Raza <ikhurramraza@gmail.com>
2024-01-02 01:31:03 +0100

    feat: add default theme for fzf popup

--- scripts/switch_session.sh
+++ scripts/switch_session.sh
@@ -4,6 +4,12 @@ SCRIPTS_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 source "$SCRIPTS_DIRECTORY/helpers.sh"
 
+# Use dracula theme
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
 main() {
   local new_session_script="$(get-tmux-option "@jumper-new-session-script" "$SCRIPTS_DIRECTORY/create_new_session.sh")"
 
