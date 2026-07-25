c77005db3742992a16ad4c0f772dbbf293ff2752
Khurram Raza <ikhurramraza@gmail.com>
2026-07-25 13:13:23 +0100

    style: set fzf popup background to black

--- scripts/switch_session.sh
+++ scripts/switch_session.sh
@@ -4,9 +4,8 @@ SCRIPTS_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 source "$SCRIPTS_DIRECTORY/helpers.sh"
 
-# Use dracula theme
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=bg+:#000000,bg:#000000,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
